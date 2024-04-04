38a34a261b2c09b930bef6aab2f641ecd515b9cb
Manpreet Kaur <manpreetanand006@gmail.com>
2024-04-04 16:14:31 -0400

    Update bill_splitter.py

--- bill_splitter.py
+++ bill_splitter.py
@@ -3,34 +3,43 @@ friends_dict = {}
 number_of_friends = int(input("Enter the number of friends joining (including you):"))
 
 if number_of_friends <= 0:
-    print("No one is joining for the party")
+      print("No one is joining for the party!")
+
+elif number_of_friends >=20:
+      print("Maximum number of friends cannot be more than 20")
+
 else:
-    print("Enter the name of every friend (including you) each on a new line:")
-    for i in range(number_of_friends):
-        name = input()
-        friends_dict[name] = 0
+      print("Enter the name of every friend (including you) each on a new line:")
+      for i in range(number_of_friends):
+            name = input()
+            friends_dict[name] = 0
 
 
-total_bill_value = int(input("Enter the total Bill Value: "))
+      total_bill_value = int(input("Enter the total Bill Value: "))
 
-lucky_feature = input('Do you want to use the "Who is Lucky" feature? Write Yes/No: ').lower()
 
-if lucky_feature == "no":
-    print("No one is going to be lucky")
-    split_bill_value = round(total_bill_value / number_of_friends, 2)
-    friends_dict = {key : split_bill_value for key in friends_dict}
-    print(friends_dict)
+      while True :
+            lucky_feature = input('Do you want to use the "Who is Lucky" feature? Write Yes/No: ').lower()
 
-elif lucky_feature == "yes":
-    random_bill_value = round(total_bill_value / (number_of_friends -1), 2)
-    lucky_person = random.choice(list(friends_dict.keys()))   
-    print(lucky_person, "is the lucky one!")
-    for key in friends_dict:
-      if key == lucky_person:
-          friends_dict[key] = 0
-      else: 
-            friends_dict[key] = random_bill_value
-    print(friends_dict)
-    
-else:
-    print('Please reply with "Yes" or "No"')
+            if lucky_feature == "no":
+                  print("No one is going to be lucky!")
+                  split_bill_value = round(total_bill_value / number_of_friends, 2)
+                  friends_dict = {key : split_bill_value for key in friends_dict}
+                  print(friends_dict)
+                  break
+
+            elif lucky_feature == "yes":
+                  random_bill_value = round(total_bill_value / (number_of_friends -1), 2)
+                  lucky_person = random.choice(list(friends_dict.keys()))   
+                  print(lucky_person, "is the lucky one!")
+                  for key in friends_dict:
+                        if key == lucky_person:
+                              friends_dict[key] = 0
+                        else: 
+                              friends_dict[key] = random_bill_value
+                  print(friends_dict)
+                  break
+
+            else:
+                  print('Please reply with "Yes" or "No"')
+            
